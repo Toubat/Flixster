ba48903c11d0e1d8e40266f2c1e43732293bb437
Toubat <57741529+Toubat@users.noreply.github.com>
2021-02-03 19:48:49 -0500

    change bg color & style

--- app/src/main/java/com/example/flixster/MainActivity.java
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
 
         RecyclerView rvMovies = findViewById(R.id.rvMovies);
         // Initialize movies list
